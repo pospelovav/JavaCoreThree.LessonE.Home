ab13de717168485cde923e0624a71d89af4f1eb3
pospelovav <shnurokvtaige@yandex.ru>
2020-11-28 22:25:28 +0300

    Семафор в туннеле

--- src/main/java/Tunnel.java
+++ src/main/java/Tunnel.java
@@ -1,19 +1,27 @@
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+
+    Semaphore queueCar;
+
+    public Tunnel(int maximumCars) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        queueCar = new Semaphore(maximumCars);
     }
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                queueCar.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                queueCar.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
